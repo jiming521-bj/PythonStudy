298727a3c9bade89a2ab4ef3c692a323d895da35
Administrator <jiming581@gmail.com>
2023-08-12 19:51:45 +0800

    Changes

--- unit_2/fun_test.py
+++ unit_2/fun_test.py
@@ -3,6 +3,9 @@
 # @Author   : JiMing
 # @File     : fun_test.py
 # @SoftWare : PyCharm
+from functools import reduce
+
+
 def number_power(number, n=1):  # 该n中的1值为默认参数
     """
     求一个数的n次方值
@@ -44,6 +47,32 @@ def add_start(L=None):
     return L
 
 
+def calc(numbers):
+    """
+    计算列表中元素的平方值的总和
+    :param numbers: 元素列表
+    :return: 总和
+    """
+    # 定义一个临时存放总和的临时变量
+    result = 0
+
+    for number in numbers:
+        result = result + number * number
+
+    return result
+
+
+def list_reduce_calc(x, y):
+    """
+    使用reduce函数求迭代数字
+    :param x:
+    :param y:
+    :return:
+    """
+    # 计数器
+    return x * 10 + y
+
+
 if __name__ == '__main__':
     # 测试3的5次方结果
     print(f"3的5次方的结果为: {number_power(3, 5)}")
@@ -55,3 +84,9 @@ if __name__ == '__main__':
     # 不可变参数的例子
     for j in range(3):
         print(add_start())  # 每次调用函数会重新定义L
+
+    # 列表中元素的总和
+    print(calc([1, 2, 3, 4, 5]))
+    print(reduce(list_reduce_calc, [1, 2, 3, 4, 5]))
+
+# 总结： 定义默认参数要牢记一点：默认参数必须是不可变对象 比如 str int None
